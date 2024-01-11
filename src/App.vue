4a83ead67e082606897d24e1f9d497003f4168b1
Mathieu <mathieu.barbey18@gmail.com>
2024-01-11 15:00:21 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import menuNav from './components/menuNav.vue'
+</script>
+
+<template>
+    <header>
+      <div>
+        <menuNav />
+      </div>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
