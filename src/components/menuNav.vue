47352ef65cfe053f1d7975a096a05ffdfb39d47d
Mathieu <mathieu.barbey18@gmail.com>
2024-01-12 15:29:00 +0100

    header_completed

--- src/components/menuNav.vue
+++ src/components/menuNav.vue
@@ -6,9 +6,9 @@
     <div id="top_page">
         <a href="../../index.html"><img id="home_picture" src="../../public/home_icon.png" alt="icone planete"></a>
         <nav>
-            <li><a id="top_menu" href="" i>A propos de moi</a></li>
-            <li><a id="top_menu" href="">Mes creations</a></li>
-            <li><a id="top_menu" href="">Contact</a></li>
+            <li><a id="top_menu" href="" i><img id="header_icon" src="../../public/satellite.png" alt="satellite"> A propos de moi</a></li>
+            <li><a id="top_menu" href=""><img id="header_icon" src="../../public/satellite.png" alt="satellite"> Mes creations</a></li>
+            <li><a id="top_menu" href=""><img id="header_icon" src="../../public/satellite.png" alt="satellite"> Contact</a></li>
         </nav>
     </div>
 </template>
@@ -20,21 +20,25 @@
 #top_page{
     margin-top: 20px;
     margin-left: 20px;
+    display: flex;
+    align-items: center;
 }
 
 #home_picture{
     width: 60px;
     float: left;
+    margin-right: 40px;
 }
 
 nav{
     display: flex;
     flex-direction: row;
-    justify-content: space-evenly;
-    
-    /* margin-right: 400px; */
-    padding-top: 15px;
+    margin-right: 400px;
     list-style: none;
+}
+
+li{
+    padding-left: 20px;
 }
 
 a{
@@ -42,6 +46,14 @@ a{
     font-family: 'Trispace';
     font-size: 1.5em;
     color: white;
+    display: flex;
+    align-items: center;
+}
+
+#header_icon{
+    width: 50px;
+    filter: invert(100);
+    background-color: #20567300;
 }
 
 #top_menu{
