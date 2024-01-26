ca43102825ae2dfd09c46c3dc7d958dcc2e2fcbe
Mathieu <mathieu.barbey18@gmail.com>
2024-01-26 16:08:03 +0100

    error message

--- src/views/errorPage.vue
+++ src/views/errorPage.vue
@@ -1,8 +1,31 @@
-<script>
-import menuNav from '../components/menuNav.vue'
-</script>
-
 <template>
-    <menuNav />
-    <p>Error</p>
+    <div id="error-message">
+        <h1 id="error-title">Error 404</h1>
+        <p id="error-text">you traveled in another galaxy.</p>
+        <p id="error-text">Come back to Earth.</p>
+    </div>
 </template>
+
+<style>
+
+#error-message{
+  background-color: #205673;
+  font-family: 'Trispace';
+  color: white;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px;
+  border-radius: 50px;
+}
+
+#error-title{
+    background-color: #205673;
+}
+
+#error-text{
+    background-color: #205673;
+}
+
+</style>
